e95400b3dd6077e76a1d3acda54c63570d6a6122
gur90 <annettgur@gmail.com>
2023-04-26 00:01:39 +0300

    fix

--- src/test/java/okhttp/AddNewContactTests.java
+++ src/test/java/okhttp/AddNewContactTests.java
@@ -17,18 +17,19 @@ public class AddNewContactTests {
 
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
     String token = "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJST0xFX1VTRVIiXSwic3ViIjoiYW5uZXR0Z3VyKzFAcmFtYmxlci5ydSIsImlzcyI6IlJlZ3VsYWl0IiwiZXhwIjoxNjgyNTI0ODM4LCJpYXQiOjE2ODE5MjQ4Mzh9.ayBWuG-7ojoPfnquDBTm_QBtX6-yaA3oNOgC4fXaSgo";
+
     @Test
     public void addNewContactTests() throws IOException {
-        int i=new Random().nextInt(1000)+1000;
-        ContactDto contactDto= ContactDto.builder()
+        int i = new Random().nextInt(1000) + 1000;
+        ContactDto contactDto = ContactDto.builder()
                 .name("Oli")
                 .lastName("Voor")
-                .email("kan"+i+"@gmail.co")
-                .phone("1234567890"+i)
+                .email("kan" + i + "@gmail.co")
+                .phone("1234567890" + i)
                 .address("Keln")
                 .description("dert")
                 .build();
-        RequestBody body= RequestBody.create(gson.toJson(contactDto), JSON);
+        RequestBody body = RequestBody.create(gson.toJson(contactDto), JSON);
 
         Request request = new Request.Builder()
                 .url("https://contactapp-telran-backend.herokuapp.com/v1/contacts")
@@ -38,15 +39,16 @@ public class AddNewContactTests {
 
         Response response = client.newCall(request).execute();
         Assert.assertTrue(response.isSuccessful());
-        Assert.assertEquals(response.code(),200);
-        ContactResponseDto resDto= gson.fromJson(response.body().string(), ContactResponseDto.class);
+        Assert.assertEquals(response.code(), 200);
+        ContactResponseDto resDto = gson.fromJson(response.body().string(), ContactResponseDto.class);
         System.out.println(resDto.getMessage());
 
     }
+
     @Test
     public void addNewContact1Tests() throws IOException {
 
-        ContactDto contactDto= ContactDto.builder()
+        ContactDto contactDto = ContactDto.builder()
                 .name("Ann")
                 .lastName("Voor")
                 .email("kana@gmail.co")
@@ -54,7 +56,7 @@ public class AddNewContactTests {
                 .address("Keln")
                 .description("dert")
                 .build();
-        RequestBody body= RequestBody.create(gson.toJson(contactDto), JSON);
+        RequestBody body = RequestBody.create(gson.toJson(contactDto), JSON);
 
         Request request = new Request.Builder()
                 .url("https://contactapp-telran-backend.herokuapp.com/v1/contacts")
@@ -64,23 +66,23 @@ public class AddNewContactTests {
 
         Response response = client.newCall(request).execute();
         Assert.assertTrue(response.isSuccessful());
-        Assert.assertEquals(response.code(),200);
-        ContactResponseDto resDto= gson.fromJson(response.body().string(), ContactResponseDto.class);
+        Assert.assertEquals(response.code(), 200);
+        ContactResponseDto resDto = gson.fromJson(response.body().string(), ContactResponseDto.class);
         System.out.println(resDto.getMessage());
 
     }
+
     @Test
     public void addNewContactNegativeTests() throws IOException {
-        int i=new Random().nextInt(1000)+1000;
-        ContactDto contactDto= ContactDto.builder()
-                .name("")
+        int i = new Random().nextInt(1000) + 1000;
+        ContactDto contactDto = ContactDto.builder()
                 .lastName("Voor")
-                .email("kan"+i+"@gmail.co")
+                .email("kan" + i + "@gmail.co")
                 .phone("12345678")
                 .address("Keln")
                 .description("dert")
                 .build();
-        RequestBody body= RequestBody.create(gson.toJson(contactDto), JSON);
+        RequestBody body = RequestBody.create(gson.toJson(contactDto), JSON);
 
         Request request = new Request.Builder()
                 .url("https://contactapp-telran-backend.herokuapp.com/v1/contacts")
@@ -90,24 +92,24 @@ public class AddNewContactTests {
 
         Response response = client.newCall(request).execute();
         Assert.assertFalse(response.isSuccessful());
-        Assert.assertEquals(response.code(),  400);
-        ErrorDto errorDto= gson.fromJson(response.body().string(), ErrorDto.class);
-        Assert.assertEquals(errorDto.getError(),"Bad Request");
+        Assert.assertEquals(response.code(), 400);
+        ErrorDto errorDto = gson.fromJson(response.body().string(), ErrorDto.class);
+        Assert.assertEquals(errorDto.getError(), "Bad Request");
         System.out.println(errorDto.getMessage());
     }
 
     @Test
     public void addNewContactUnauthUserNegativeTest() throws IOException {
-        int i=new Random().nextInt(1000)+1000;
-        ContactDto contactDto= ContactDto.builder()
+        int i = new Random().nextInt(1000) + 1000;
+        ContactDto contactDto = ContactDto.builder()
                 .name("")
                 .lastName("Voor")
-                .email("kan"+i+"@gmail.co")
-                .phone("1234567890"+i)
+                .email("kan" + i + "@gmail.co")
+                .phone("1234567890" + i)
                 .address("Keln")
                 .description("dert")
                 .build();
-        RequestBody body= RequestBody.create(gson.toJson(contactDto), JSON);
+        RequestBody body = RequestBody.create(gson.toJson(contactDto), JSON);
 
         Request request = new Request.Builder()
                 .url("https://contactapp-telran-backend.herokuapp.com/v1/contacts")
@@ -121,33 +123,7 @@ public class AddNewContactTests {
         Assert.assertEquals(response.code(), 401);
 
         ErrorDto errorDto = gson.fromJson(response.body().string(), ErrorDto.class);
-        Assert.assertEquals(errorDto.getError(),"Unauthorized");
+        Assert.assertEquals(errorDto.getError(), "Unauthorized");
     }
-    @Test
-    public void addNewContactDublicateNegativeTest() throws IOException {
-        ContactDto contactDto= ContactDto.builder()
-                .name("Ann")
-                .lastName("Voor")
-                .email("kana@gmail.co")
-                .phone("12345678901")
-                .address("Keln")
-                .description("dert")
-                .build();
-        RequestBody body= RequestBody.create(gson.toJson(contactDto), JSON);
 
-        Request request = new Request.Builder()
-                .url("https://contactapp-telran-backend.herokuapp.com/v1/contacts")
-                .addHeader("Authorization", token)
-                .post(body)
-                .build();
-
-
-        Response response = client.newCall(request).execute();
-       //Assert.assertFalse(response.isSuccessful());
-        //Assert.assertEquals(response.code(), 409);
-
-        ErrorDto errorDto = gson.fromJson(response.body().string(), ErrorDto.class);
-        System.out.println(errorDto.getMessage());
-        //Assert.assertEquals(errorDto.getError(),"Unauthorized");
-    }
 }
